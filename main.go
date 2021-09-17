ef9c5330456d491b8ca46c1ada18c06976ace7a3
n4to4 <n4to4k@gmail.com>
2021-09-17 23:52:51 +0900

    snapshot

--- main.go
+++ main.go
@@ -2,32 +2,40 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"text/template"
 )
 
 //go:embed tmpl/*
 var f embed.FS
 
+type values struct {
+	User      string
+	Extension string
+}
+
 func main() {
-	if len(os.Args) != 1 {
+	if len(os.Args) != 2 {
 		log.Fatal("extension name required, got none")
 	}
 	extension := os.Args[1]
 
+	// gh extension create foo
 	cmd := exec.Command("gh", "extension", "create", extension)
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to run gh command: %v\n", err)
 	}
 
 	ghExtension := "gh-" + extension
 	if err := os.Chdir(ghExtension); err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to Chdir: %v\n", err)
 	}
 
-	// gh extension create foo
 	// apply templates
-	fmt.Println("")
+	tmpl := template.Must(template.ParseFS(f, "tmpl/*.tmpl"))
+
+	vals := values{"n4to4", ghExtension}
+	tmpl.ExecuteTemplate(os.Stdout, "go.mod.tmpl", vals)
 }
